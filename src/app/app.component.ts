ee04cf2338f24ac03a25e467d708d568e1dc9eff
cxllics <oddsquad0325@gmail.com>
2020-03-27 16:25:47 -0700

    5 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,18 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
     <div class="app-container">
       <mat-card>
         <mat-toolbar>
-          cxllics-dev
+        A new website is coming soon! 
+        This one is just temporary.
         </mat-toolbar><br>
         <mat-card-content>
-          <p>Hello there!<br>
-          Welcome to cxllics-dev!<br>
-          This website is for cxllics's projects & more!</p>
+          <p>Hey there, welcome to cxllics.dev! Our website is currently in BETA so please mind the dust. Thanks! :)</p>
         </mat-card-content>
       </mat-card><br>
 
       <mat-card>
         <mat-card-title>
-          Test | Title
+          Projects
         </mat-card-title>
-        <mat-card-content>
-          <p>Test | Description
-        </mat-card-content>
-        <mat-card-subtitle>
-         Posted on: 3/27/2020
-        </mat-card-subtitle>
       </mat-card>
 
       <mat-card>
@@ -41,38 +34,38 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
         </mat-card-subtitle>
       </mat-card>
 
-      <mat-card>
-        <mat-card-title>
-          Material Icons 
-        </mat-card-title>
-        <mat-card-content>
-          <p>To add to the project in case you're gonna be using it,
-          only have to add the code below (link tag) to your 
-          <strong>index.html</strong> file, section head</p>
-        </mat-card-content>
-        <mat-card-subtitle>
-          < link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        </mat-card-subtitle>
-      </mat-card>
 
       <mat-card class="example-card">
         <mat-card-title>
          Forums
         </mat-card-title>
         <mat-card-subtitle>
-          Hello! The Forums are currently only open developers of this site. 
+          Hello! The forums are currently only open to developers of this site. 
+        </mat-card-subtitle>
+      </mat-card>
+      
+            <mat-card class="example-card">
+        <mat-card-title>
+         Forum Post #1
+        </mat-card-title>
+        <mat-card-subtitle>
+         Hello there! This is our first forum post! Seems like it works, doesn't it? :D
         </mat-card-subtitle>
         <mat-card-header>
           <div mat-card-avatar class="example-header-image"></div>
           <mat-card-title>cxllics</mat-card-title>
-          <mat-card-subtitle>Hello there! This is our first forum post! Seems like it works, doesn't it? :D</mat-card-subtitle>
+          <mat-card-subtitle>Posted on: 3/27/20202</mat-card-subtitle>
         </mat-card-header>
         <mat-card-actions>
           <button #likeBtn mat-button (click)="onLike()"></button>
         </mat-card-actions>
       </mat-card>
     </div>
+   
+ 
   `,
+
+
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
